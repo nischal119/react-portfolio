789c60c39bdeced6a92a4792b292d464cb75968d
nischal119 <dhungelnischal7@gmail.com>
2025-07-17 12:07:22 +0545

    remove download button

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,5 @@
 import React, { useEffect, memo, useMemo, useState } from "react";
-import {
-  FileText,
-  Code,
-  Award,
-  Globe,
-  ArrowUpRight,
-  Sparkles,
-} from "lucide-react";
+import { Code, Award, Globe, ArrowUpRight, Sparkles } from "lucide-react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -254,18 +247,6 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a
-                href="https://drive.google.com/file/d/10Tx1xDmV2LXph88bp7_shq4bFmLuUnDY/view?usp=sharing"
-                className="w-full lg:w-auto"
-              >
-                <button
-                  data-aos="fade-up"
-                  data-aos-duration="800"
-                  className="w-full lg:w-auto sm:px-6 py-2 sm:py-3 rounded-lg bg-gradient-to-r from-[#6366f1] to-[#a855f7] text-white font-medium transition-all duration-300 hover:scale-105 flex items-center justify-center lg:justify-start gap-2 shadow-lg hover:shadow-xl animate-bounce-slow"
-                >
-                  <FileText className="w-4 h-4 sm:w-5 sm:h-5" /> Download CV
-                </button>
-              </a>
               <a href="#Portofolio" className="w-full lg:w-auto">
                 <button
                   data-aos="fade-up"
